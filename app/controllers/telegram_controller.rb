6f541bf0ea9a8051b6053ea633813fa39f74cfdc
dongideon <Gideon.gur@klarna.com>
2021-06-20 12:18:29 +0200

    sticker handle

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -11,36 +11,39 @@ class TelegramController < ApplicationController
             telegramResponseCreator = TelegramResponseCreator.new(chatId)
 
             User.new(first_name: params[:message][:chat][:first_name], last_name: params[:message][:chat][:last_name], user_name: params[:message][:chat][:username], message: params[:message][:text]).save
-            theWord = params[:message][:text]
-            theWordSimplefy = theWord.downcase.delete(' ').delete('/')
-            grammarRules = [
-                Adjektivdeklination.new,
-                AkkusativOrDativ.new,
-                Personalpronomen.new,
-                Possessivartikel.new,
-                Indefinitpronomen.new
-            ]
-            linkers = [
-                ReversoLinker.new,
-                DictccLinker.new
-            ]
-            case
-                when correctGrammarRule =  grammarRules.find { |grammarRule| (grammarRule.keyboardOptions + [grammarRule.displayName]).include?(theWord) }
-                    correctGrammarRule.messageLogic(telegramResponseCreator, theWord)
+            if theWord = params[:message][:text]
+                theWordSimplefy = theWord.downcase.delete(' ').delete('/')
+                grammarRules = [
+                    Adjektivdeklination.new,
+                    AkkusativOrDativ.new,
+                    Personalpronomen.new,
+                    Possessivartikel.new,
+                    Indefinitpronomen.new
+                ]
+                linkers = [
+                    ReversoLinker.new,
+                    DictccLinker.new
+                ]
+                case
+                    when correctGrammarRule =  grammarRules.find { |grammarRule| (grammarRule.keyboardOptions + [grammarRule.displayName]).include?(theWord) }
+                        correctGrammarRule.messageLogic(telegramResponseCreator, theWord)
 
-                when correctLinker =  linkers.find { |link| link.displayName == theWord }
-                    telegramResponseCreator.textResponse(correctLinker.instructions)
-        
-                when theWordSimplefy == 'h' || theWordSimplefy == 'hilfe' || theWordSimplefy == 'help' || theWordSimplefy == 'start'
-                    buttons = (grammarRules + linkers).map { |option| option.displayName }
-                    telegramResponseCreator.keyboardButtons(buttons, '🐌 WÄHLE!')
-                
-                when correctLinker =  linkers.find { |link| theWordSimplefy.include?(link.commandChar) }
-                    wordToUse = theWord.split.last
-                    correctLinker.messageLogic(telegramResponseCreator, wordToUse)
+                    when correctLinker =  linkers.find { |link| link.displayName == theWord }
+                        telegramResponseCreator.textResponse(correctLinker.instructions)
+            
+                    when theWordSimplefy == 'h' || theWordSimplefy == 'hilfe' || theWordSimplefy == 'help' || theWordSimplefy == 'start'
+                        buttons = (grammarRules + linkers).map { |option| option.displayName }
+                        telegramResponseCreator.keyboardButtons(buttons, '🐌 WÄHLE!')
                     
-                when theWordSimplefy == ('🐌')
-                    telegramResponseCreator.textResponse('❤️')
+                    when correctLinker =  linkers.find { |link| theWordSimplefy.include?(link.commandChar) }
+                        wordToUse = theWord.split.last
+                        correctLinker.messageLogic(telegramResponseCreator, wordToUse)
+                        
+                    when theWordSimplefy == ('🐌')
+                        telegramResponseCreator.textResponse('❤️')
+                end
+            else
+                telegramResponseCreator.textResponse('🐌❤️')
             end
         end
         
