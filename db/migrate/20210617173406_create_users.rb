a2f2df962d6de3892617ea11e9bcd5c1945bda8b
dongideon <gur.gideon@gmail.com>
2023-02-05 20:14:09 +0100

    remove schema, making heroku happy

--- db/migrate/20210617173406_create_users.rb
+++ db/migrate/20210617173406_create_users.rb
@@ -1,12 +1,12 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :users do |t|
-      t.string :user_name
-      t.string :first_name
-      t.string :last_name
-      t.text :message
+# class CreateUsers < ActiveRecord::Migration[6.1]
+#   def change
+#     create_table :users do |t|
+#       t.string :user_name
+#       t.string :first_name
+#       t.string :last_name
+#       t.text :message
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_173406) do
+# ActiveRecord::Schema.define(version: 2021_06_17_173406) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "first_name"
-    t.string "last_name"
-    t.text "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+#   create_table "users", force: :cascade do |t|
+#     t.string "user_name"
+#     t.string "first_name"
+#     t.string "last_name"
+#     t.text "message"
+#     t.datetime "created_at", precision: 6, null: false
+#     t.datetime "updated_at", precision: 6, null: false
+#   end
 
-end
+# end
